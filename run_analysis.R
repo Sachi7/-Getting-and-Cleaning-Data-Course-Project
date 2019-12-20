8b0a40d805cade386f4efccb4d166d8badfc63f7
Sachi7 <57734189+Sachi7@users.noreply.github.com>
2019-12-20 10:48:40 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,13 @@
-# Checking if file/folder exists and downloading data if it does not exist
+## Checking if file/folder exists and downloading data if it does not exist
    filename <- "getdata_projectfiles_UCI HAR Dataset.zip"
-   
    if (!file.exists(filename)){
         fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
         download.file(fileURL, filename, method="curl")
    }  
-   
    if (!file.exists("UCI HAR Dataset")) { 
         unzip(filename) 
    }
+
 
 ## Setting up the environment
    library(data.table)
